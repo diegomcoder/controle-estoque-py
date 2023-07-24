cfb69b0b9478b1802c7d6152420ac3d860514bbd
diegomdsv <mdsidiego@gmail.com>
2023-07-24 00:11:06 -0300

    Update files

--- controle-estoque.py
+++ controle-estoque.py
@@ -3,19 +3,49 @@
 
 stock = []
 
-startOptions = {
-    "1": "Iniciar com estoque vazio",
-    "2": "Carregar estoque demonstrativo",
-    "3": "Fechar o controle de estoque"}
+# START OF THE PROGRAM
+def main():
 
-print('\n______________________________________________________\nCONTROLE DE ESTOQUE')
-print(f'\n(1) {startOptions["1"]}\n(2) {startOptions["2"]}\n(3) {startOptions["3"]}\n')
+    options = {
+        "1": "Iniciar com estoque vazio",
+        "2": "Carregar estoque demonstrativo",
+        "3": "Fechar o controle de estoque"}
 
-while True:
-    userInput = input('INSIRA O NÚMERO DO COMANDO DESEJADO: ')
-    if userInput in startOptions.keys():
-        break
+    print('\n______________________________________________________\nCONTROLE DE ESTOQUE')
+    print(f'\n(1) {options["1"]}\n(2) {options["2"]}\n(3) {options["3"]}\n')
 
+    while True:
+        userInput = input('INSIRA O NÚMERO DO COMANDO DESEJADO: ')
+        if userInput in options.keys():
+            break
+
+    if userInput == "3":
+        return
+    
+    elif userInput == "2":
+        loadDemoStock()
+
+    else:
+        registerProduct()
+
+# LOAD DEMONSTRATIVE STOCK
+def loadDemoStock():
+    print("\n______________________________________________________\nBEM VINDO AO CONTROLE DE ESTOQUE!")
+    print("\nESTOQUE DEMONSTRATIVO CARREGADO!\nSaldo de estoque: 439 produtos\nQuantidade de categorias: 25\nSaldo monetário de estoque: R$ 315.087,00\n")
+
+# REGISTER FIRST PRODUCTS
+def registerProduct():
+    options = {
+        "1": "Sim, cadastrar um produto",
+        "2": "Voltar ao menu anterior",
+        "3": "Fechar o controle de estoque"}
+    
+    print("\n______________________________________________________\nBEM VINDO AO CONTROLE DE ESTOQUE!")
+    print("\nCADASTRE O SEU PRIMEIRO PRODUTO\n")
+    print(f'\n(1) {options["1"]}\n(2) {options["2"]}\n(3) {options["3"]}\n')
+
+# CALL THE START OF THE PROGRAM
+main()
 
 
 """
