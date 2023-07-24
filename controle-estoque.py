3cd0965e20e950b101d72bebff6fc93381385952
diegomdsv <mdsidiego@gmail.com>
2023-07-23 23:46:20 -0300

    Update files

--- controle-estoque.py
+++ controle-estoque.py
@@ -1,6 +1,24 @@
 # To open an external terminal: Ctrl + Shift + C
 # to run this python file: python controle-estoque.py
 
+stock = []
+
+startOptions = {
+    "1": "Iniciar com estoque vazio",
+    "2": "Carregar estoque demonstrativo",
+    "3": "Fechar o controle de estoque"}
+
+print('\n______________________________________________________\nCONTROLE DE ESTOQUE')
+print(f'\n(1) {startOptions["1"]}\n(2) {startOptions["2"]}\n(3) {startOptions["3"]}\n')
+
+while True:
+    userInput = input('INSIRA O NÚMERO DO COMANDO DESEJADO: ')
+    if userInput in startOptions.keys():
+        break
+
+
+
+"""
 category = input('Categoria do produto: ')
 code = int(input('Código do produto: '))
 name = input('Nome do produto: ')
@@ -16,3 +34,4 @@ print(f'Descrição do produto: {description}')
 print(f'Preço do produto: {price}')
 print('###############################')
 print()
+"""
