cd77e609e26dae72337da63a855638c03b972b23
diegomdsv <mdsidiego@gmail.com>
2023-07-24 22:56:51 -0300

    Update files

---
+++ controle_estoque.py
@@ -0,0 +1,44 @@
+# To open an external terminal: Ctrl + Shift + C
+# to run this python file: python controle-estoque.py
+import json
+
+with open("demo_stock.json", encoding='utf-8') as my_json:
+    demo_stock = json.load(my_json)
+
+stock = []
+
+# START OF THE PROGRAM
+def main():
+    userName = input("\nQual o seu nome? ")
+
+    print(f'\n______________________________________________________\n\nSeja bem vindo (a) {userName} 😉')
+    print('\n(1) Iniciar com estoque vazio\n(2) Carregar estoque demonstrativo\n(3) Fechar o controle de estoque\n')
+
+    while True:
+        userInput = input('INSIRA O NÚMERO DO COMANDO DESEJADO: ')
+        if userInput in ["1", "2", "3"]:
+            break
+
+    if userInput == "3":
+        print("\n\nATÉ MAIS! 😊\n")
+        return
+    elif userInput == "2":
+        loadDemoStock()
+    else:
+        registerProduct()
+
+# LOAD DEMONSTRATIVE STOCK
+def loadDemoStock():
+    
+    print("\n______________________________________________________\nBEM VINDO AO CONTROLE DE ESTOQUE!")
+    print("\nESTOQUE DEMONSTRATIVO CARREGADO!\nSaldo de estoque: 439 produtos\nQuantidade de categorias: 25\nSaldo monetário de estoque: R$ 315.087,00\n")
+    print(demo_stock)
+
+# REGISTER FIRST PRODUCTS
+def registerProduct():
+    print("\n______________________________________________________\nBEM VINDO AO CONTROLE DE ESTOQUE!")
+    print("\nCADASTRE O SEU PRIMEIRO PRODUTO\n")
+    print('\n(1) Sim, cadastrar um produto\n(2) Voltar ao menu anterior\n(3) Fechar o controle de estoque\n')
+
+# CALL THE START OF THE PROGRAM
+main()
