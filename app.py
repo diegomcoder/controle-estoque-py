5ecb68376f8fc17ea7bc2c001a20aaf26b7a020a
Diego Medeiros <mdsidiego@gmail.com>
2023-09-17 20:46:40 -0300

    delete new branch files

--- app.py
+++
@@ -1,5 +0,0 @@
-# app.py: This is the main entry point of your application. It could set up the database and handle the main loop of your application, asking for user input and responding to it.
-
-from database import database
-
-database.db_init()
--- views/category_view.py
+++
@@ -1,1 +0,0 @@
-# views/product_view.py and views/category_view.py: These modules could contain functions for displaying products and categories to the user, and for handling user input related to products and categories.
